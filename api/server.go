157a6443f75def13ffc034cca0d4aa93b5e4dc31
Johnpaul M <johnpaul.muoneme@dreamlabs.com.ng>
2024-06-21 22:39:52 +0100

    feat: add DB Migrations at code level

--- api/server.go
+++ api/server.go
@@ -3,12 +3,16 @@ package api
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 
 	db "github.com/SwiftFiat/SwiftFiat-Backend/db/sqlc"
 	"github.com/SwiftFiat/SwiftFiat-Backend/models"
 	"github.com/SwiftFiat/SwiftFiat-Backend/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 // / If there's a better place to access this
@@ -30,6 +34,20 @@ func NewServer(envPath string) *Server {
 	conn, err := sql.Open(c.DBDriver, utils.GetDBSource(c, c.DBName))
 	if err != nil {
 		panic(fmt.Sprintf("Could not load DB: %v", err))
+	}
+
+	m, err := migrate.New(
+		"file://db/migrations",
+		utils.GetDBSource(c, c.DBName),
+	)
+	if err != nil {
+		log.Fatalf("Unable to instantiate the database schema migrator - %v", err)
+	}
+
+	if err := m.Up(); err != nil {
+		if err != migrate.ErrNoChange {
+			log.Fatalf("Unable to migrate up to the latest database schema - %v", err)
+		}
 	}
 
 	q := db.New(conn)
--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require github.com/gin-gonic/gin v1.10.0
 
 require (
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
@@ -36,6 +38,8 @@ require (
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/golang-migrate/migrate v3.5.4+incompatible
+	github.com/golang-migrate/migrate/v4 v4.17.1
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
