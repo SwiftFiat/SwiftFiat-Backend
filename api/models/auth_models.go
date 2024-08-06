f8e45f529ae1e7b77e87584fd3839e4350089c30
Johnpaul M <johnpaul.muoneme@dreamlabs.com.ng>
2024-08-06 11:16:50 +0100

    feat: add pin having evaluation

--- api/models/auth_models.go
+++ api/models/auth_models.go
@@ -39,6 +39,7 @@ type UserResponse struct {
 	Email       string    `json:"email"`
 	PhoneNumber string    `json:"phone_number"`
 	Verified    bool      `json:"verified"`
+	HasPin      bool      `json:"has_pin"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
