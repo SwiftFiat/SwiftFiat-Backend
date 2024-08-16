d3928e5a2fa65f0af43ee2bd02e66be3ce3d8f28
Johnpaul M <johnpaul.muoneme@dreamlabs.com.ng>
2024-08-16 23:14:02 +0100

    chore: edit error messages

--- api/apistrings/auth_strings.go
+++ api/apistrings/auth_strings.go
@@ -3,8 +3,8 @@ package apistrings
 const (
 	UserNotFound              = "user or account does not exist"
 	UserDetailsAlreadyCreated = "email or phone number already exists"
-	InvalidPhone              = "invalid phone number"
-	InvalidEmail              = "invalid email address"
+	InvalidPhone              = "invalid phone number, please use a standard phone number"
+	InvalidEmail              = "invalid email address, please check submitted email address"
 	InvalidPhoneEmailInput    = "please enter a valid email and password"
 	IncorrectEmailPass        = "incorrect email or password"
 	ServerError               = "a server error occurred, please try again later"
