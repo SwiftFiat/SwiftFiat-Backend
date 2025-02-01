7dd04fc0d1f514237e8f89993063a1ddf963c76f
Johnpaul M <johnpaul@pluhg.com>
2025-02-01 18:04:18 +0100

    feat: standardize data transactions

---
+++ api/models/bill_model.go
@@ -0,0 +1,8 @@
+package models
+
+type BillVariation struct {
+	VariationCode   string `json:"variation_code"`
+	Name            string `json:"name"`
+	VariationAmount string `json:"variation_amount"`
+	FixedPrice      string `json:"fixed_price"`
+}
