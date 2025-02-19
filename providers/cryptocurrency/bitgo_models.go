9470959d4f5bd2648b771f5a3c9a8a5d6995688f
Johnpaul M <johnpaul@pluhg.com>
2025-02-19 08:00:02 +0100

    feat: add coin label mapping

--- providers/cryptocurrency/bitgo_models.go
+++ providers/cryptocurrency/bitgo_models.go
@@ -1,5 +1,62 @@
 package cryptocurrency
 
+var supportedCoinLabels = map[string]string{
+	"BCH":   "Bitcoin Cash",
+	"bch":   "Bitcoin Cash",
+	"BNB":   "Binance Coin",
+	"bnb":   "Binance Coin",
+	"BTC":   "Bitcoin",
+	"btc":   "Bitcoin",
+	"DOGE":  "Dogecoin",
+	"doge":  "Dogecoin",
+	"DOT":   "Polkadot",
+	"dot":   "Polkadot",
+	"ETH":   "Ethereum",
+	"eth":   "Ethereum",
+	"LINK":  "Chainlink",
+	"link":  "Chainlink",
+	"LTC":   "Litecoin",
+	"ltc":   "Litecoin",
+	"MATIC": "Polygon",
+	"matic": "Polygon",
+	"SHIB":  "Shiba Inu",
+	"shib":  "Shiba Inu",
+	"SOL":   "Solana",
+	"sol":   "Solana",
+	"TRON":  "TRON",
+	"tron":  "TRON",
+	"UNI":   "Uniswap",
+	"uni":   "Uniswap",
+	"USDC":  "USD Coin",
+	"usdc":  "USD Coin",
+	"USDT":  "Tether",
+	"usdt":  "Tether",
+	"XLM":   "Stellar",
+	"xlm":   "Stellar",
+	"XRP":   "XRP",
+	"xrp":   "XRP",
+
+	// Testnet
+	"tbtc":   "Test Bitcoin",
+	"tbtc4":  "Test Bitcoin",
+	"tbch":   "Test Bitcoin Cash",
+	"tbnb":   "Test Binance Coin",
+	"tdoge":  "Test Dogecoin",
+	"tdot":   "Test Polkadot",
+	"teth":   "Test Ethereum",
+	"tlink":  "Test Chainlink",
+	"tltc":   "Test Litecoin",
+	"tmatic": "Test Polygon",
+	"tshib":  "Test Shiba Inu",
+	"tsol":   "Test Solana",
+	"ttron":  "Test TRON",
+	"tuni":   "Test Uniswap",
+	"tusdc":  "Test USD Coin",
+	"tusdt":  "Test Tether",
+	"txlm":   "Test Stellar",
+	"txrp":   "Test XRP",
+}
+
 type WalletCreationModel struct {
 	Label                           string `json:"label"`                           // The label for the wallet
 	Passphrase                      string `json:"passphrase"`                      // The wallet's passphrase
@@ -51,4 +108,12 @@ type WalletCoinSpecific struct {
 	PendingChainInitialization *bool         `json:"pendingChainInitialization,omitempty"`
 	RootAddress                *string       `json:"rootAddress,omitempty"`
 	TrustedTokens              []interface{} `json:"trustedTokens,omitempty"`
+}
+
+func GetWalletLabel(coin string) string {
+	label, ok := supportedCoinLabels[coin]
+	if !ok {
+		return coin
+	}
+	return label
 }
